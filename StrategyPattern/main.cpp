6af18422ebadaeac8a441e467e04758cb7dcb770
pangkuanxin <351477478@qq.com>
2021-06-10 21:02:56 +0800

    修改测试例子

--- StrategyPattern/main.cpp
+++ StrategyPattern/main.cpp
@@ -1,23 +1,27 @@
 #include "MallardDuck.h"
 #include "ModelDuck.h"
-#include "Squeak.h"
+#include "FlyWithWings.h"
 #include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::shared_ptr<Duck> pMallarDuck = std::make_shared<MallardDuck>();
+    shared_ptr<Duck> pMallarDuck = std::make_shared<MallardDuck>();
     pMallarDuck->display();
     pMallarDuck->performFly();
     pMallarDuck->performQuack();
 
-    std::cout << "\n";
+    cout << endl;
 
-    std::shared_ptr<Duck> pModelDuck = std::make_shared<ModelDuck>();
+    shared_ptr<Duck> pModelDuck = std::make_shared<ModelDuck>();
     pModelDuck->display();
     pModelDuck->performFly();
     pModelDuck->performQuack();
-    pModelDuck->setQuackBehavior(new Squeak);
-    pModelDuck->performQuack();
+
+    cout << endl;
+
+    pModelDuck->setFlyBehavior(new FlyWithWings);
+    pModelDuck->performFly();
 
     return 0;
 }
